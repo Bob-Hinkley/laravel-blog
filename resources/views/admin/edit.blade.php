721e863dbfb23d8df89a17bea9d9c754dfb43abd
Bob-Hinkley <bob.hinkley1@gmail.com>
2017-05-23 14:16:35 -0500

    Updated Readme

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <div class="row">
         <div class="col-md-12">
+          {{-- Notice -> Method set to POST! --}}
             <form action="{{ route('admin.update') }}" method="post">
                 <div class="form-group">
                     <label for="title">Title</label>
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 
 Route::get('/', function () {
     return view('blog.index');
-})->name('blog.index ');
+})->name('blog.index');
 
 // (First is path, and second is what's executed)
 Route::get('post/{id}', function(){
@@ -24,14 +24,28 @@ Route::get('about', function () {
     return view('other.about');
 })->name('other.about');
 
-Route::get('admin', function () {
-    return view('admin.index');
-})->name('admin.index');
 
-Route::get('admin/create', function () {
-    return view('admin.create');
-})->name('admin.create');
+// Grouping together Admin routs
+Route::group(['prefix' => 'admin'], function(){
 
-Route::get('admin/edit/{id}', function () {
-    return view('admin.edit');
-})->name('admin.edit');
+  Route::get('', function () {
+      return view('admin.index');
+  })->name('admin.index');
+
+  Route::get('create', function () {
+      return view('admin.create');
+  })->name('admin.create');
+
+  Route::post('create', function(){
+    return "It works";
+  })->name('admin.create');
+
+  Route::get('edit/{id}', function () {
+      return view('admin.edit');
+  })->name('admin.edit');
+
+  Route::post('edit', function(){
+    return "It works";
+  })->name('admin.update');
+
+});
